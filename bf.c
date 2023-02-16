0da1d08aa4df9abb2ddfac65aa6cf4f80b1bd35c
Fynn Muermanns <fynn.m@live.de>
2023-02-17 00:45:31 +0100

    print to stdout unless --intermediate option is specified; refactored some stuff

--- bf.c
+++ bf.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define PATH_LIMIT       100  // limit input path length for safety reasons
+#define BF_FILE_EXT      ".bf"
+#define INTERMEDIATE_EXT ".c"
+
 // brainfuck commands
 #define NEXT       '>'
 #define PREV       '<'
@@ -22,76 +26,18 @@ void help() {
          "-i, --intermediate    generate intermediate C code\n\n");
 }
 
-void generate_intermediate() {
-}
-
 bool _is_bf_file(const char *file) {
   // check if input file ends in ".bf"
-  size_t offset = strlen(file) - 3;
-  return strncmp(file + offset, ".bf", 3) == 0;
+  size_t offset = strnlen(file, PATH_LIMIT) - 3;
+  return strncmp(file + offset, BF_FILE_EXT, 3) == 0;
 }
 
-int parse_args(int argc, char **argv,
-               char **bf_file, char **input_bf_str,
-               int *tape_size) {
-  // parse the command line arguments
-  if (argc == 1) {
-    help();
-    return 0;
-  }
-
-  const struct option long_opts[] = {
-    {"help", no_argument, NULL, 'h'},
-    {"intermediate", no_argument, NULL, 'i'},
-    {"parse", required_argument, NULL, 'p'},
-    {"tapesize", required_argument, NULL, 'n'},
-    {NULL, no_argument, NULL, 0}};
-
-  int o = 0;
-
-  while ((o = getopt_long(argc, argv, ":hin:", long_opts, NULL)) != -1) {
-    switch (o) {
-      case 'h' :
-        help();
-        break;
-      case 'i' :
-        generate_intermediate();
-        break;
-      case 'p' :
-        *input_bf_str = optarg;
-        break;
-      case 'n' :
-        *tape_size = atoi(optarg);
-        break;
-      case '?' :
-      default :
-        printf("Unkown option -%c\n", (char)optopt);
-        return 1;
-    }
-  }
-
-  if (!!input_bf_str) {
-    // only look for an input file if
-    // no input string was provided
-    bool is_bf_file = _is_bf_file(argv[argc - 1]);
-    if (optind == argc || !is_bf_file) {
-      printf("No .bf file provided.\n");
-      return 1;
-    }
-
-    *bf_file = argv[argc - 1];
-  }
-
-  return 0;
-}
-
-FILE* __init_out_file(int tape_size) {
+int __init_out_file(FILE *out, int tape_size) {
   // initialize the output intermediate C file
   // (either as a file or a stream depending on user input)
-  FILE *out = fopen("parsed_bf.c", "w");
 
   if (!out) {
-    return NULL;
+    return 1;
   }
 
   fprintf(out,
@@ -101,7 +47,7 @@ FILE* __init_out_file(int tape_size) {
          "  unsigned char *i = tape;\n",
          tape_size);
 
-  return out;
+  return 0;
 }
 
 int __close_out_file(FILE *out, int *indent) {
@@ -147,7 +93,8 @@ void __parse_char(FILE *out, char c, int *indent) {
   }
 }
 
-int parse_bf_string(const char *bf_string, int tape_size) {
+int parse_bf_string(const char *bf_string, int tape_size,
+                    bool generate_intermediate) {
   // parse the provided string as if it was a ".bf" file
   // and create intermediate C code from it
 
@@ -155,7 +102,16 @@ int parse_bf_string(const char *bf_string, int tape_size) {
     return 1;
   }
 
-  FILE *out = __init_out_file(tape_size);
+  FILE *out = NULL;
+  if (generate_intermediate) {
+    out = fopen("bf_program.c", "w");
+  } else {
+    out = stdout;
+  }
+
+  int fail = __init_out_file(out, tape_size);
+  if (fail) return fail;
+
   int indent = 2;
 
   const size_t n_chars = strlen(bf_string);
@@ -166,7 +122,34 @@ int parse_bf_string(const char *bf_string, int tape_size) {
   return __close_out_file(out, &indent);
 }
 
-int parse_bf_file(const char *bf_file_path, int tape_size) {
+char* __get_bf_file_name(const char *bf_file_path) {
+  const int PATH_LENGTH = strnlen(bf_file_path, PATH_LIMIT);
+  int start = 0;
+  for (int i = PATH_LENGTH; i-- > 0;) {
+    if (bf_file_path[i] == '/') {
+      start = i + 1;
+      break;
+    }
+  }
+
+  const int CURR_FILE_NAME_LENGTH = PATH_LENGTH - start;
+  const int INTERMEDIATE_FILE_NAME_LENGTH = CURR_FILE_NAME_LENGTH -
+                                            strlen(BF_FILE_EXT) +
+                                            strlen(INTERMEDIATE_EXT);
+  char *bf_file_name = (char*) malloc(INTERMEDIATE_FILE_NAME_LENGTH *
+                                      sizeof(char) + 1);
+  memcpy(bf_file_name, bf_file_path + start,
+         CURR_FILE_NAME_LENGTH - strlen(BF_FILE_EXT));
+
+  bf_file_name[INTERMEDIATE_FILE_NAME_LENGTH - 2] = '.';
+  bf_file_name[INTERMEDIATE_FILE_NAME_LENGTH - 1] = 'c';
+  bf_file_name[INTERMEDIATE_FILE_NAME_LENGTH] = '\0';
+
+  return bf_file_name;
+}
+
+int parse_bf_file(const char *bf_file_path, int tape_size,
+                  bool generate_intermediate) {
   // parse the ".bf" file and create intermediate C code from it
 
   if (!bf_file_path) {
@@ -180,42 +163,96 @@ int parse_bf_file(const char *bf_file_path, int tape_size) {
     return 1;
   }
 
-  FILE *out = __init_out_file(tape_size);
-  int indent = 2;
+  FILE *out = NULL;
+  char *bf_file_name = NULL;
+  int fail = 0;
+  if (generate_intermediate) {
+    bf_file_name = __get_bf_file_name(bf_file_path);
+    out = fopen(bf_file_name, "w");
+  } else {
+    out = stdout;
+  }
 
+  fail = __init_out_file(out, tape_size);
+  if (fail) return fail;
+
+  int indent = 2;
   char c;
+
   do {
     c = fgetc(bf_file);
     __parse_char(out, c, &indent);
   } while (c != EOF);
 
-  int fail = fclose(bf_file);
-  if (fail) {
-    return 1;
-  }
+  fail = fclose(bf_file);
+  if (fail) return fail;
+
+  free(bf_file_name);
 
   return __close_out_file(out, &indent);
 }
 
-int main(int argc, char **argv) {
+
+int parse_args(int argc, char **argv) {
+  // parse the command line arguments
+  if (argc == 1) {
+    help();
+    return 0;
+  }
+
   char *bf_file_path = NULL, *bf_str = NULL;
   int tape_size = 30000;
+  bool generate_intermediate = false;
 
-  int fail = parse_args(argc, argv, &bf_file_path,
-                        &bf_str, &tape_size);
-  if (fail) {
-    return 1;
+  const struct option long_opts[] = {
+    {"help", no_argument, NULL, 'h'},
+    {"intermediate", no_argument, NULL, 'i'},
+    {"parse", required_argument, NULL, 'p'},
+    {"tapesize", required_argument, NULL, 'n'},
+    {NULL, no_argument, NULL, 0}};
+
+  int o = 0;
+
+  while ((o = getopt_long(argc, argv, ":hin:", long_opts, NULL)) != -1) {
+    switch (o) {
+      case 'h' :
+        help();
+        return 0;
+      case 'i' :
+        generate_intermediate = true;
+        break;
+      case 'p' :
+        bf_str = optarg;
+        break;
+      case 'n' :
+        tape_size = atoi(optarg);
+        break;
+      case '?' :
+      default :
+        printf("Unkown option -%c\n", (char)optopt);
+        return 1;
+    }
   }
 
-  if (bf_str) {
-    fail = parse_bf_string(bf_str, tape_size);
+  if (!bf_str) {
+    // only look for an input file if
+    // no input string was provided
+    bool is_bf_file = _is_bf_file(argv[argc - 1]);
+    if (optind == argc || !is_bf_file) {
+      printf("No .bf file provided."
+             "Please provide a filethat ends in .bf\n"
+             "and that the path length doesn't exceed %d characters.\n",
+             PATH_LIMIT);
+      return 1;
+    }
+
+    bf_file_path = argv[argc - 1];
+    return parse_bf_file(bf_file_path, tape_size, generate_intermediate);
   } else {
-    fail = parse_bf_file(bf_file_path, tape_size);
+    return parse_bf_string(bf_str, tape_size, generate_intermediate);
   }
+}
 
-  if (fail) {
-    return 1;
-  }
-
-  return 0;
+int main(int argc, char **argv) {
+  return parse_args(argc, argv);
 }
