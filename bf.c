f2f882423f60444a95c7185c37ed9ce115004d5a
Fynn Muermanns <fynn.m@live.de>
2023-02-26 20:58:29 +0100

    refactored a bunch of stuff; real interpreter works now

--- bf.c
+++ bf.c
@@ -4,10 +4,18 @@
 #include <string.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <stddef.h>
 
 #define PATH_LIMIT       100  // limit input path length for safety reasons
 #define BF_FILE_EXT      ".bf"
 #define INTERMEDIATE_EXT ".c"
+#define MAX_TAPE_SIZE    1000000000  // limit tape size
+
+#ifdef _WIN32
+#define PATH_SEPARATOR   '\\'
+#else
+#define PATH_SEPARATOR   '/'
+#endif
 
 // brainfuck commands
 #define NEXT       '>'
@@ -26,7 +34,7 @@ void* __resize(void *arr, size_t n_bytes) {
   new = realloc(arr, n_bytes);
 
   if (!new) {
-    printf("Reallocation failed. Aborting...\n");
+    fprintf(stderr, "Reallocation failed. Aborting...\n");
     abort();
   }
 
@@ -52,7 +60,8 @@ int __init_out_file(FILE *out, int tape_size) {
          "#include <stdio.h>\n\n"
          "int main() {\n"
          "  unsigned char tape[%d] = {0};\n"
-         "  unsigned char *i = tape;\n",
+         "  unsigned char *i = tape;\n"
+         "  char c;\n",
          tape_size);
 
   return 0;
@@ -74,7 +83,7 @@ char* __get_file_name(const char *bf_file_path) {
   const int PATH_LENGTH = strnlen(bf_file_path, PATH_LIMIT);
   int start = 0;
   for (int i = PATH_LENGTH; i-- > 0;) {
-    if (bf_file_path[i] == '/') {
+    if (bf_file_path[i] == PATH_SEPARATOR) {
       start = i + 1;
       break;
     }
@@ -111,70 +120,66 @@ long __get_file_size(FILE *file) {
   return size;
 }
 
-// parse the current character and add it
-// to the code array if it is a bf command;
-// also store loop opening addresses, which will later
-// be needed to do conditional jumps in the interpreter function
-void __parse_char(char c, unsigned char *code, long *idx,
-                  int *open_loop_i, int *close_loop_i, int *mx_loops,
-                  unsigned char ***open_loops) {
+/*
+   parse the current character and add it
+   to the code array if it is a bf command;
+   also store loop opening addresses, which will later
+   be needed to do conditional jumps in the interpreter
+*/
+long __parse_char(char c, unsigned char *code, long idx,
+                  int *open_loop_i, int *close_loop_i,
+                  int *mx_loops, long *open_loops) {
   switch (c) {
     case NEXT :
-      code[(*idx)++] = NEXT;
+      code[idx] = NEXT;
       break;
     case PREV :
-      code[(*idx)++] = PREV;
+      code[idx] = PREV;
       break;
     case INC :
-      code[(*idx)++] = INC;
+      code[idx] = INC;
       break;
     case DEC :
-      code[(*idx)++] = DEC;
+      code[idx] = DEC;
       break;
     case PRINT :
-      code[(*idx)++] = PRINT;
+      code[idx] = PRINT;
       break;
     case INSERT :
-      code[(*idx)++] = INSERT;
+      code[idx] = INSERT;
       break;
     case START_LOOP :
-    {
-      // add idx/address of START_LOOP instruction to array
-      // and resize array if necessary
+      code[idx] = START_LOOP;
 
+      // add idx of START_LOOP instruction to array
+      // and resize array if necessary
       if (*open_loop_i == *mx_loops) {
         *mx_loops *= 2;
-        *open_loops = __resize(*open_loops,
-                               *mx_loops * sizeof(unsigned char*));
+        open_loops = __resize(open_loops, (*mx_loops) * sizeof(long));
       }
 
-      unsigned char *current_addr = code + (*idx);
-      (*open_loops)[(*open_loop_i)++] = current_addr;
-
-      code[(*idx)++] = START_LOOP;
-      assert(*current_addr == START_LOOP);
+      open_loops[(*open_loop_i)++] = idx;
       break;
-    }
     case CLOSE_LOOP :
-      code[(*idx)++] = CLOSE_LOOP;
+      code[idx] = CLOSE_LOOP;
       (*close_loop_i)++;
       break;
   }
+
+  return ++idx;
 }
 
 // execute the bf instructions
 void __execute_instructions(unsigned char *code_ptr, const unsigned char *end,
-                            unsigned char *tape_ptr, unsigned char **open_loops,
-                            unsigned char **close_loops, int total_loops) {
-  char curr_bf_instruction;
-  int loop_i = -1, stack_ptr = -1;
-  int stack[total_loops];  // keep track of loop nesting
+                            unsigned char *tape_ptr, unsigned char **loops) {
+  char curr_bf_instruction, inp;
+  unsigned char *start = code_ptr;
+  long count = 0;
 
   // parse the current bf instruction and execute
   // the appropriate C instruction
   while (code_ptr < end) {
     curr_bf_instruction = *code_ptr;
-    printf(" %c ", curr_bf_instruction);
 
     switch (curr_bf_instruction) {
       case NEXT :
@@ -193,34 +198,35 @@ void __execute_instructions(unsigned char *code_ptr, const unsigned char *end,
         putchar(*tape_ptr);
         break;
       case INSERT :
-        *tape_ptr = getchar();
+        if ((inp = getchar()) != EOF) *tape_ptr = inp;
         break;
       case START_LOOP :
-        // <<[++[--]<<[--]]++[<]
-
         // if a loop is opened, jump to the end of the loop
         // and evaluate the loop condition;
         // if it's true, jump back to this address + 1;
         // if it's false, simply keep going
-        ++loop_i;
-        ++stack_ptr;
-        stack[stack_ptr] = loop_i;
-        printf("stack open = %d\n", stack[stack_ptr]);
-        code_ptr = close_loops[stack[stack_ptr]];
+        ptrdiff_t close_offset = code_ptr - start;
+        assert(close_offset >= 0);
+        code_ptr = loops[close_offset];
+
+        assert(code_ptr);
         assert(*code_ptr == CLOSE_LOOP);
-        continue;  // don't increment the code ptr
+        continue;
       case CLOSE_LOOP :
         if (*tape_ptr != 0) {  // bf loop condition
           // jump back to loop opening
-          printf("stack = %d\n", stack[stack_ptr]);
-          code_ptr = open_loops[stack[stack_ptr]];
-          assert(*code_ptr == START_LOOP);
-        } else {
-          --stack_ptr;
+          ptrdiff_t open_offset = code_ptr - start;
+          assert(open_offset > 0);
+          code_ptr = loops[open_offset] + 1;
+
+          assert(code_ptr);
+          assert(*(code_ptr-1) == START_LOOP);
+          continue;
         }
     }
 
     ++code_ptr;
+    count++;
   }
 }
 
@@ -246,8 +252,9 @@ void __generate_instruction(FILE *out, char c, int *indent) {
                             "putchar(*i);");
       break;
     case INSERT :
-      fprintf(out, "%*s\n", (int)(strlen("*i = getchar();") + *indent),
-                            "*i = getchar();");
+      fprintf(out, "%*s\n",
+              (int)(strlen("if ((c = getchar()) != EOF) *i = c;") + *indent),
+              "if ((c = getchar()) != EOF) *i = c;");
       break;
     case START_LOOP :
       fprintf(out, "%*s\n", (int)(strlen("while (*i != 0) {") + *indent),
@@ -261,44 +268,68 @@ void __generate_instruction(FILE *out, char c, int *indent) {
   }
 }
 
-// iterate over all loop opening addresses
-// and find their corresponding closing addresses
-void __find_loop_closing_addresses(unsigned char **open_loops,
-                                   int total_loops,
-                                   unsigned char **close_loops) {
+/*
+   iterate over all loop opening addresses
+   and find their corresponding closing addresses;
+   addresses will be written into the "loops" array,
+   which serves as a "key-value" storage that returns
+   the closing loop address for every opening loop
+   address and vice versa
+*/
+int __get_loop_addresses(long *open_loops, int total_loops,
+                         unsigned char *code, long size,
+                         unsigned char **loops) {
   unsigned char curr_instruction;
 
   for (int i = 0; i < total_loops; i++) {
-    unsigned char *curr_loop_ptr = open_loops[i] + 1;
-    int loop_c = 1;
+    // start iterating from 1st instruction after START_LOOP instruction
+    long curr_open_loop_start = open_loops[i];
 
-    while (1) {
-      curr_instruction = *curr_loop_ptr;
+    int loop_c = 1;  // count how often a new loop is opened/closed
+
+    for (long i = curr_open_loop_start + 1; i < size; i++) {
+      curr_instruction = code[i];
 
       if (curr_instruction == START_LOOP) {
         ++loop_c;
       } else if (curr_instruction == CLOSE_LOOP) {
         --loop_c;
+
         if (loop_c == 0) {
-          assert(*curr_loop_ptr == CLOSE_LOOP);
-          close_loops[i] = curr_loop_ptr;
+          // add CLOSE_LOOP address to idx where START_LOOP instruction sits,
+          // so the interpreter can directly jump there
+          // (and do the opposite for the START_LOOP case)
+          long offset_to_closing_loop = i;
+          long offset_to_opening_loop = curr_open_loop_start;
+
+          loops[curr_open_loop_start] = code + offset_to_closing_loop;
+          loops[i] = code + offset_to_opening_loop;
           break;
         }
       }
-
-      ++curr_loop_ptr;
     }
+
+    if (loop_c != 0) return 1;
   }
+
+  return 0;
 }
 
 // execute the generated C code
-void execute_generated_code(const char *file_name) {
+void execute_generated_code(const char *file_name, bool remove) {
   const int COMMAND_BUFFER_SIZE = 100;
   char cmd[COMMAND_BUFFER_SIZE];
 
-  snprintf(cmd, COMMAND_BUFFER_SIZE,
-           "gcc -O3 -o tmp %s && ./tmp && rm -f tmp",
-           file_name);
+  if (remove) {
+    snprintf(cmd, COMMAND_BUFFER_SIZE,
+             "gcc -O3 -o tmp %s && ./tmp && rm -f tmp %s",
+             file_name, file_name);
+  } else {
+    snprintf(cmd, COMMAND_BUFFER_SIZE,
+             "gcc -O3 -o tmp %s && ./tmp && rm -f tmp",
+             file_name);
+  }
+
   system(cmd);
 }
 
@@ -324,69 +355,72 @@ int run_bf_code(bool parse_str, const char *bf_string,
     size = __get_file_size(bf_file);
   }
 
-  unsigned char* code = (unsigned char*) malloc(size);
+  unsigned char *code = (unsigned char*) malloc(size);
+  unsigned char **loops = (unsigned char**) malloc(size *
+                                                  sizeof(unsigned char*));
 
   long idx = 0;
-  int open_loop_i = 0, close_loop_i = 0, mx_loops = 10, fail = 0;
+  int open_loop_i = 0, close_loop_i = 0;  // counters for opening/closing loops
+  int mx_loops = 10, fail = 0;
 
-  // stores addresses where loops start
-  unsigned char **open_loops = (unsigned char**)
-                                malloc(mx_loops * sizeof(unsigned char*));
+  // stores indices where loops start
+  long *open_loops = (long*) malloc(mx_loops * sizeof(long));
 
   if (parse_str) {
     // read all bf instructions from the input string
     // and write them to the code array
     for (size_t i = 0; i < size; i++) {
-      __parse_char(bf_string[i], code, &idx, &open_loop_i, &close_loop_i,
-                   &mx_loops, &open_loops);
+      idx = __parse_char(bf_string[i], code, idx, &open_loop_i,
+                         &close_loop_i, &mx_loops, open_loops);
     }
   } else {
+    // read all bf instructions from the input file
+    // and write them to the code array
     char c;
 
     if (!bf_file) {
-      printf("%s can't be opened.\n", input_file);
+      fprintf(stderr, "%s can't be opened.\n", input_file);
       return 1;
     }
+
     do {
       c = fgetc(bf_file);
-      __parse_char(c, code, &idx, &open_loop_i,
-                   &close_loop_i, &mx_loops, &open_loops);
+      idx = __parse_char(c, code, idx, &open_loop_i,
+                         &close_loop_i, &mx_loops, open_loops);
     } while (c != EOF);
+
     fclose(bf_file);
   }
 
   if (open_loop_i > close_loop_i) {
-    printf("More open loop instructions ('[') than "
+    fprintf(stderr, "More open loop instructions ('[') than "
            "close loop instructions (']').\n");
     fail = 1;
   } else if (open_loop_i < close_loop_i) {
-    printf("Less loop instructions ('[') than "
+    fprintf(stderr, "Less loop instructions ('[') than "
            "close loop instructions (']').\n");
     fail = 1;
   }
 
   if (!fail) {
-    // store addresses where loops are closed
-    unsigned char **close_loops = (unsigned char**)
-                                  malloc(open_loop_i * sizeof(unsigned char*));
-
     // find corresponding closing address for every loop opening address
-    __find_loop_closing_addresses(open_loops, open_loop_i, close_loops);
+    fail = __get_loop_addresses(open_loops, open_loop_i, code, size, loops);
+    free(open_loops);
+
+    if (fail) return fail;
 
     // zero-initialize the tape (default size: 30000)
     unsigned char *tape = (unsigned char*) calloc(tape_size, sizeof(char));
-    const unsigned char *end = code + size;
+    const unsigned char* end = code + size;  // marks the end of the bf code
 
     // run the actual interpreter
-    __execute_instructions(code, end, tape, open_loops,
-                           close_loops, open_loop_i);
+    __execute_instructions(code, end, tape, loops);
 
     free(tape);
-    free(close_loops);
   }
 
   free(code);
-  free(open_loops);
+  free(loops);
 
   return fail;
 }
@@ -399,8 +433,8 @@ int parse_bf_string(const char *bf_string, int tape_size,
   }
   const size_t n_chars = strlen(bf_string);
   int fail = 0;
-  //           vvvvvv    temporary until actual interpreter works
-  if (compile || true) {
+
+  if (compile) {
     // create intermediate C code
     const char *file_name = "bf_program.c";
     FILE *out = fopen(file_name, "w");
@@ -418,8 +452,6 @@ int parse_bf_string(const char *bf_string, int tape_size,
     fail = __close_out_file(out, &indent);
 
     if (fail) return fail;
-
-    if (!compile) execute_generated_code(file_name);
   } else {
     // run interpreter
     fail = run_bf_code(true, bf_string, NULL, tape_size);
@@ -437,13 +469,12 @@ int parse_bf_file(const char *bf_file_path, int tape_size,
     return fail;
   }
 
-  //           vvvvvv    temporary until actual interpreter works
-  if (compile || true) {
+  if (compile) {
     // generate intermediate C code
 
     FILE *bf_file = fopen(bf_file_path, "r");
     if (!bf_file) {
-      printf("%s can't be opened.\n", bf_file_path);
+      fprintf(stderr, "%s can't be opened.\n", bf_file_path);
       return 1;
     }
 
@@ -468,12 +499,10 @@ int parse_bf_file(const char *bf_file_path, int tape_size,
     fail = fclose(bf_file);
     if (fail) return fail;
 
-    if (!compile) execute_generated_code(file_name);
-
     free(file_name);
   } else {
     // run interpreter
-    run_bf_code(false, NULL, bf_file_path, tape_size);
+    fail = run_bf_code(false, NULL, bf_file_path, tape_size);
   }
 
   return fail;
@@ -483,7 +512,7 @@ int parse_bf_file(const char *bf_file_path, int tape_size,
 
 // print usage help
 void help() {
-  printf("Usage: ./bf [options] file.bf\n\n"
+  fprintf(stderr, "Usage: ./bf [options] file.bf\n\n"
          "Options:\n"
          "-p, --parse  STR      parse STR as brainfuck code\n"
          "-n, --tapesize N      specify the size of the tape (default: 30000)\n"
@@ -524,11 +553,18 @@ int parse_args(int argc, char **argv) {
         break;
       case 'n' :
         tape_size = atoi(optarg);
+        if (tape_size > MAX_TAPE_SIZE) {
+          fprintf(stderr, "Specified tape size is too large "
+                 "(max is set to 1B cells or 1GB RAM).\n"
+                 "Please increase MAX_TAPE_SIZE if you "
+                 "need more than 1B cells.\n");
+          return 1;
+        }
         break;
       case '?' :
       case ':' :
       default :
-        printf("Unkown option -%c\n", (char)optopt);
+        fprintf(stderr, "Unkown option -%c\n", (char)optopt);
         return 1;
     }
   }
@@ -538,8 +574,8 @@ int parse_args(int argc, char **argv) {
     // no input string was provided
     bool is_bf_file = __is_bf_file(argv[argc - 1]);
     if (optind == argc || !is_bf_file) {
-      printf("No .bf file provided."
-             "Please provide a file that ends in .bf\n"
+      fprintf(stderr, "No .bf file provided.\n"
+             "Please provide a file that ends in \".bf\"\n"
              "and ensure that the path length doesn't exceed %d characters.\n",
              PATH_LIMIT);
       return 1;
